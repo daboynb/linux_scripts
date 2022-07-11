f86f5ce26497836df4431e25fd1ebf88c46367ea
daboynb <106079917+daboynb@users.noreply.github.com>
2022-07-11 09:27:52 +0200

    Update non_free.sh

--- non_free.sh
+++ non_free.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Enable non free repo
 echo "Adding non-free repo"
 text="deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
