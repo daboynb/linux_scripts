1222779446a973f9a29c15e52a175c00227b5a22
daboynb <106079917+daboynb@users.noreply.github.com>
2022-07-21 08:46:16 +0200

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-openssl req -new -x509 -newkey rsa:2048 -keyout /home/"$USER"/Nvidia.key -outform DER -out /home/"$USER"/Nvidia.der -nodes -days 36500 -subj "/CN=Graphics Drivers"
-sudo mokutil --import /home/"$USER"/Nvidia.der
-echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf; sudo update-initramfs -u
-sudo reboot
+
+# Dependencies
+sudo apt-get install linux-headers-$(uname -r) build-essential libglvnd-dev pkg-config -y
+
+# Menu
+mainmenu() {
+    echo -ne "
+1) Create keys and import with mok 
+2) Install drivers
+0) Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+
+    1)
+            openssl req -new -x509 -newkey rsa:2048 -keyout /home/"$USER"/Nvidia.key -outform DER -out /home/"$USER"/Nvidia.der -nodes -days 36500 -subj "/CN=Graphics Drivers"
+            sudo mokutil --import /home/"$USER"/Nvidia.der
+            echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf; sudo update-initramfs -u
+            read -p "Now when it reboot chose enroll mok --> continue --> yes --> insert the password --> reboot"
+            sudo reboot
+        ;;
+    2)
+            read -e -p "Enter Nvidia xxx.run path : " file
+            eval xyz=${abc[i]}
+            eval file=$file
+            echo "$file" | tr -d ''
+            chmod +x $file
+            sudo $file --module-signing-secret-key=/home/"$USER"/Nvidia.key --module-signing-public-key=/home/"$USER"/Nvidia.der
+        ;;
+    0)      
+            echo "Bye bye."
+            exit 0
+            ;;
+    *)
+        echo "Wrong option."
+        mainmenu
+        ;;
+    esac
+}
+
+mainmenu
