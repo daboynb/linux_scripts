184b4449fb4d1984a503e643b0636ed53f14fbeb
daboynb <106079917+daboynb@users.noreply.github.com>
2022-07-25 15:45:56 +0200

    Delete 4k.sh

--- 4k.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo " Make grub bigger"
-sudo sed -i 's/#GRUB_GFXMODE="640x480"/GRUB_GFXMODE="640x480"/g' /etc/default/grub'
-sudo update-grub
-echo "reboot to apply"
-echo "Completed"
