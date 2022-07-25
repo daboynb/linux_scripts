1fd8d300ede2ab30b68fdf9e65ad2d753785fb5d
daboynb <106079917+daboynb@users.noreply.github.com>
2022-07-25 15:47:04 +0200

    Delete fix_usb.sh

--- fix_usb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "fix usb problems"
-sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&acpi=force irqpoll /' /etc/default/grub
-sudo update-grub
-echo "reboot to apply"
-echo "Completed"
