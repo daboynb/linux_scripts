1cb9dda0cf3dc0da047445ee95c428410f7b1f3c
daboynb <106079917+daboynb@users.noreply.github.com>
2022-07-26 19:25:44 +0200

    Update bypass_ubuntu_upgrade.sh

--- bypass_ubuntu_upgrade.sh
+++ bypass_ubuntu_upgrade.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+sudo sed -i 's/continue/pass/g' /usr/lib/python3/dist-packages/UpdateManager/Core/MetaRelease.py
+sed -i 's/hirsute/jammy/g' /etc/apt/sources.list
+sudo apt-get update
+echo "Upgrade distro"
+sudo do-release-upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install -f -y
+sudo apt-get autoremove --purge -y
+
+
+#!/bin/bash
 echo "Updating"
 sudo apt-get update
 sudo apt-get upgrade -y
