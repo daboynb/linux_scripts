7ff05abf5e48ddc2d25af6d6807ed17823296bd6
daboynb <106079917+daboynb@users.noreply.github.com>
2022-08-17 11:01:06 +0200

    Create sign_vmware.sh

---
+++ sign_vmware.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ask for sudo privileges
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+# Fix vmmon not found or not loaded
+sudo vmware-modconfig --console --install-all 
+
+# Sign modules
+openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=VMware/"
+sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon)
+sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet)
+mokutil --import MOK.der
+
+
+
