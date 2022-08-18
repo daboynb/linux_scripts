17d07a31760a1fdceda3a15243436be96b429b46
daboynb <106079917+daboynb@users.noreply.github.com>
2022-08-18 19:38:19 +0200

    Update sign_vmware.sh

--- sign_vmware.sh
+++ sign_vmware.sh
@@ -11,6 +11,8 @@ openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.d
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon)
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet)
 mokutil --import MOK.der
+read -p "Now, after pressing enter it will reboot; at boot chose enroll mok --> continue --> yes --> insert the password --> reboot"
+sudo reboot
 
 
 
