c1d098318863698bd646653be77a65809a995441
daboynb <106079917+daboynb@users.noreply.github.com>
2022-08-18 19:38:43 +0200

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -22,7 +22,7 @@ Choose an option:  "
             openssl req -new -x509 -newkey rsa:2048 -keyout /home/"$USER"/Nvidia.key -outform DER -out /home/"$USER"/Nvidia.der -nodes -days 36500 -subj "/CN=Graphics Drivers"
             sudo mokutil --import /home/"$USER"/Nvidia.der
             echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf; sudo update-initramfs -u
-            read -p "Now when it reboot chose enroll mok --> continue --> yes --> insert the password --> reboot"
+            read -p "Now, after pressing enter it will reboot; at boot chose enroll mok --> continue --> yes --> insert the password --> reboot"
             sudo reboot
         ;;
     2)
