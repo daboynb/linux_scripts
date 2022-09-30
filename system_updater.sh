3e21960c548bb172931962025c98b830c85ed753
daboynb <106079917+daboynb@users.noreply.github.com>
2022-09-30 08:39:18 +0200

    Update system_updater.sh

--- system_updater.sh
+++ system_updater.sh
@@ -11,7 +11,7 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove --purge -y
 
-# Check if flatpak is installed and if yes check for updates
+# Check if flatpak is installed and check for updates
 flatpak=flatpak
 which $flatpak > /dev/null 2>&1
 if [ $? == 0 ]
@@ -19,7 +19,7 @@ then
 flatpak update -y
 fi
 
-# Check if snapd is installed and if yes check for updates
+# Check if snapd is installed and check for updates
 snapd=snapd 
 which $snapd > /dev/null 2>&1
 if [ $? == 0 ]
@@ -27,7 +27,7 @@ then
 sudo snap refresh
 fi
 
-# Check if fwupd is installed and if yes check for updates
+# Check if fwupd is installed and check for updates
 fwupdmgr=fwupdmgr 
 which $fwupdmgr > /dev/null 2>&1
 if [ $? == 0 ]
