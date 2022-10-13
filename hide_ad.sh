0a94bd42f8fedbad7a1ae360ff1ba88809f169ae
daboynb <106079917+daboynb@users.noreply.github.com>
2022-10-13 12:56:11 +0200

    Update hide_ad.sh

--- hide_ad.sh
+++ hide_ad.sh
@@ -1,2 +1,3 @@
 echo "Hide Try Ubuntu Pro beta with a free personal subscription on up to 5 machines."
-sudo ln -s -f /dev/null /etc/apt/apt.conf.d/20apt-esm-hook.conf
+sudo mv /etc/apt/apt.conf.d/20apt-esm-hook.conf /etc/apt/apt.conf.d/20apt-esm-hook.conf.bak
+sudo touch /etc/apt/apt.conf.d/20apt-esm-hook.conf
