52b79a82f5118020a4fd894c89cefac0b4100842
daboynb <106079917+daboynb@users.noreply.github.com>
2022-10-19 11:40:51 +0200

    auto

--- back_to_stock.sh
+++ back_to_stock.sh
@@ -1,12 +1,16 @@
+# Detect os codename
+codename=$(awk '/UBUNTU_CODENAME=/' /etc/os-release | sed 's/UBUNTU_CODENAME=//' | sed 's/[.]0/./')
+
+# Disale all the external repos
 cd /etc/apt/sources.list.d && sudo bash -c 'for i in *.list; do mv ${i} ${i}.disabled; done' && cd /home/"$USER"
 
- # Replace sources.list change focal to your distro codename 
-        text="deb http://archive.ubuntu.com/ubuntu/ focal main universe restricted multiverse
-deb-src http://archive.ubuntu.com/ubuntu/ focal main universe restricted multiverse
-deb http://security.ubuntu.com/ubuntu focal-security main universe restricted multiverse
-deb-src http://security.ubuntu.com/ubuntu focal-security main universe restricted multiverse
-deb http://archive.ubuntu.com/ubuntu/ focal-updates main universe restricted multiverse
-deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main universe restricted multiverse"
+# Replace sources.list  
+text="deb http://archive.ubuntu.com/ubuntu/ $codename main universe restricted multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ $codename main universe restricted multiverse
+deb http://security.ubuntu.com/ubuntu $codename-security main universe restricted multiverse
+deb-src http://security.ubuntu.com/ubuntu $codename-security main universe restricted multiverse
+deb http://archive.ubuntu.com/ubuntu/ $codename-updates main universe restricted multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ $codename-updates main universe restricted multiverse"
 
 sudo echo "$text" | sudo tee /etc/apt/sources.list
 
@@ -17,3 +21,9 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get install -f -y
 sudo apt-get autoremove --purge -y
+
+echo ""
+echo "All inside /etc/apt/sources.list.d is now disabled. Enable ONLY what you need!"
+echo ""
+echo "Example < sudo mv /etc/apt/sources.list.d/mozillateam-ubuntu-ppa-jammy.list.disabled /etc/apt/sources.list.d/mozillateam-ubuntu-ppa-jammy.list >"
+echo ""
