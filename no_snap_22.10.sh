49e82319884e31d69a6435b2000a2251b0461a50
daboynb <106079917+daboynb@users.noreply.github.com>
2022-10-21 16:43:07 +0200

    Update no_snap_22.10.sh

--- no_snap_22.10.sh
+++ no_snap_22.10.sh
@@ -3,14 +3,6 @@
 # Start
 sudo apt update
 
-sudo snap remove --purge firefox
-sudo snap remove --purge gnome-3-38-2004
-sudo snap remove --purge snap-store
-sudo snap remove --purge snapd-desktop-integration
-sudo snap remove --purge bare
-sudo snap remove --purge gtk-common-themes
-sudo snap remove --purge core20
-
 # Fix for gedit error "Failed to execute child process dbus-launch"
 sudo apt install dbus-x11 -y
 
@@ -20,7 +12,13 @@ echo "Removing snap...This will take a while"
 sudo systemctl stop snapd && sudo systemctl disable snapd
 
 # Uninstall
-sudo umount /var/snap/firefox/common/host-hunspell
+sudo snap remove --purge firefox
+sudo snap remove --purge gnome-3-38-2004
+sudo snap remove --purge snap-store
+sudo snap remove --purge snapd-desktop-integration
+sudo snap remove --purge bare
+sudo snap remove --purge gtk-common-themes
+sudo snap remove --purge core20
 sudo apt purge -y snapd 
 sudo apt purge -y gnome-software-plugin-snap 
 
