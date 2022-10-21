f27fbea5ffd2dd39c39fdb69277fb4b8e06fe62e
daboynb <106079917+daboynb@users.noreply.github.com>
2022-10-21 16:52:00 +0200

    up

--- no_snap_22.04.sh
+++ no_snap_22.04.sh
@@ -12,6 +12,13 @@ echo "Removing snap...This will take a while"
 sudo systemctl stop snapd && sudo systemctl disable snapd
 
 # Uninstall
+sudo snap remove --purge firefox
+sudo snap remove --purge snap-store
+sudo snap remove --purge snapd-desktop-integration
+sudo snap remove --purge gtk-common-themes
+sudo snap remove --purge gnome-3-38-2004
+sudo snap remove --purge core20
+sudo snap remove --purge bare
 sudo apt purge -y snapd 
 sudo apt purge -y gnome-software-plugin-snap 
 
