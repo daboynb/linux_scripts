1e730f7f24cc7270b6fddc213f672afca3e53764
daboynb <106079917+daboynb@users.noreply.github.com>
2022-11-07 15:00:44 +0100

    Update Testing.sh

--- Testing.sh
+++ Testing.sh
@@ -3,38 +3,20 @@ apt=apt
 which $apt > /dev/null 2>&1
 if [ $? == 0 ]
 then
-debian=debian
+sudo apt install pastebinit -y
 fi
 
 dnf=dnf
 which $dnf > /dev/null 2>&1
 if [ $? == 0 ]
 then
-fedora=fedora
+sudo dnf install pastebinit -y
 fi
 
 pacman=pacman
 which $pacman > /dev/null 2>&1
 if [ $? == 0 ]
 then
-arch=arch
-fi
-
-# Debian
-if [ "$apt" == "apt" ]; then
-echo "You're on Debian"
-sudo apt install pastebinit -y
-fi
-
-# Fedora 
-if [ "$dnf" == "dnf" ]; then
-echo "You're on Fedora"
-sudo dnf install pastebinit -y
-fi
-
-# Arch
-if [ "$pacman" == "pacman" ]; then
-echo "You're on Arch"
 sudo pacman -Syu pastebinit --noconfirm
 fi
 
