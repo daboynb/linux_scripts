1d2ac1bb68a353ca8b82fa27a5a476d7b088df5f
daboynb <106079917+daboynb@users.noreply.github.com>
2022-11-07 16:23:22 +0100

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-( sudo lspci ; echo "" ; uname -a ) > info.txt
+( echo "lspci" ; sudo lspci ; echo "" ; echo "uname -a" ; uname -a ; echo "" ; echo "lshw" ; sudo lshw ) > info.txt
 cat info.txt | nc termbin.com 9999
+
