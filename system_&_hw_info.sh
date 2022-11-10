28b8a5a320e8de277782494230062ecdb1164a83
daboynb <106079917+daboynb@users.noreply.github.com>
2022-11-10 14:24:05 +0100

    Update system_&_hw_info.sh

--- system_&_hw_info.sh
+++ system_&_hw_info.sh
@@ -2,4 +2,4 @@
 (printf "LSPCI\n" ; sudo lspci ; printf "\nUNAME -A\n" ; uname -a ; printf "\nLSHW\n" ; sudo lshw ; printf "\nSOURCES.LIST\n" ; sudo cat /etc/apt/sources.list) > info.txt
 output=$(cat info.txt | nc termbin.com 9999)
 clear
-echo "[+] Link to share --> $output"
+echo "[👉] Link to share --> $output"
