764ad0ceaf93308616e1078304b6a8a6ab0c5271
daboynb <106079917+daboynb@users.noreply.github.com>
2022-11-16 10:19:18 +0100

    up

--- virtualBox_guest_additions.sh
+++ virtualBox_guest_additions.sh
@@ -4,13 +4,11 @@ sudo apt update
 sudo apt install p7zip-full build-essential -y
 
 # Download and install VirtualBox Guest Additions
-mkdir /home/"$USER"/VBoxGuestAdditions
-cd /home/"$USER"/VBoxGuestAdditions
-wget http://download.virtualbox.org/virtualbox/7.0.2/VBoxGuestAdditions_7.0.2.iso
-7z x VBoxGuestAdditions*.iso 
-chmod +x ./*.run
-sudo ./*.run
-cd /home/"$USER"
+mkdir VBoxGuestAdditions
+wget -P VBoxGuestAdditions http://download.virtualbox.org/virtualbox/7.0.2/VBoxGuestAdditions_7.0.2.iso
+7z x VBoxGuestAdditions/VBoxGuestAdditions*.iso -oVBoxGuestAdditions
+chmod +x VBoxGuestAdditions/*.run
+sudo VBoxGuestAdditions/*.run
 rm -rf VBoxGuestAdditions 
 sudo adduser "$USER" vboxsf
 echo "Done"
