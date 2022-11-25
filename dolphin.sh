1acbec834a334df9f0f7024b99caf482b529c957
daboynb <106079917+daboynb@users.noreply.github.com>
2022-11-25 08:56:29 +0100

    Update dolphin.sh

--- dolphin.sh
+++ dolphin.sh
@@ -3,7 +3,7 @@
 dolphin="pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY KDE_SESSION_VERSION=5 KDE_FULL_SESSION=true dolphin"
 # Create dolphin.sh
 echo "$dolphin" >> /home/"$USER"/.dolphin.sh
-chmod +x .dolphin.sh
+chmod +x /home/"$USER"/.dolphin.sh
 # Variable for the alias
 alias="alias dp="\""/home/$USER/.dolphin.sh"\"""
 # Add the alias to .bashrc
