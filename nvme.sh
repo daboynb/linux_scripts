af79b834159cb4033fa9fdb39118d71ce79560ef
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-02 08:17:12 +0100

    Update nvme.sh

--- nvme.sh
+++ nvme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "This fix the issues with the new nvme drive"
-sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&nvme_core.default_ps_max_latency_us=0 acpi=noirq net.ifnames=0 biosdevname=0 quiet /' /etc/default/grub 
+sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&nvme_core.default_ps_max_latency_us=0 /' /etc/default/grub 
 sudo update-grub
 echo "Completed"
