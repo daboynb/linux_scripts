8fc5c8efac6c422c92274b88e6dd46b0ebd412dd
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-04 12:12:49 +0100

    Update xps_9550.sh

--- xps_9550.sh
+++ xps_9550.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Fix the dell xps 9550 issues after replacing the ssd to a nvme"
-sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&nvme_core.default_ps_max_latency_us=0 net.ifnames=0 biosdevname=0 quiet intremap=no_x2apic_optout /' /etc/default/grub
+sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&nvme_core.default_ps_max_latency_us=0 intremap=no_x2apic_optout /' /etc/default/grub
 sudo update-grub
 echo "reboot to apply"
