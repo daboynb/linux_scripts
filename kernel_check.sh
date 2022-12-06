844a4099bb957e3b7b9f05680fab35efdb3e16b7
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-06 08:58:13 +0100

    Create kernel_check.sh

---
+++ kernel_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Display the lastest kernel installed
+ls -v /boot/vmlinuz-* | tail -n 1 > text 
+# Remove some words from the output
+sed -i "s/boot\|vmlinuz-*//g" text > /dev/null 2>&1
+sed -i 's|/||g' text > /dev/null 2>&1
+installed_kernel=$(cat text)
+# Check current installed kernel
+current=$(uname -r)
+# Assign variables
+echo last installed kernel : $installed_kernel
+echo current installed kernel : $current
+# Check if the kernel is the same or not
+if [ $installed_kernel = $current ]; then
+    echo "You have the lastest kernel already installed"
+else
+  echo "You need to reboot to apply the new kernel"
+fi
+rm text
