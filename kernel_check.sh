f906542c32ec37fe0b08fc88115c26982ec2758c
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-06 09:46:56 +0100

    Update kernel_check.sh

--- kernel_check.sh
+++ kernel_check.sh
@@ -7,7 +7,7 @@ sed -i 's|/||g' text > /dev/null 2>&1
 # Assign variables
 installed_kernel=$(cat text)
 current=$(uname -r)
-# Check current installed kernel
+# Check installed kernels
 echo last installed kernel : $installed_kernel
 echo current installed kernel : $current
 # Check if the kernel is the same or not
