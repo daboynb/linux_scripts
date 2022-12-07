3c40b82ea01fb7f948b544f80f71ced836c46b65
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-07 09:15:47 +0100

    Update 4k.sh

--- 4k.sh
+++ 4k.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Make grub bigger"
-sudo sed -i 's/#GRUB_GFXMODE="640x480"/GRUB_GFXMODE="640x480"/g' /etc/default/grub
+sudo sed -i 's/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=640x480/g' /etc/default/grub
 sudo update-grub
 echo "reboot to apply"
