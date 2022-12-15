85ff47cbfa06e05d619ccf74d0d52dcba7d4bf4a
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-15 11:23:33 +0100

    Update gnome_shell.sh

--- gnome_shell.sh
+++ gnome_shell.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Enable right click touchpad and the close,minimize,maximize buttons on gnome shell"
+echo "Enable the right click touchpad, the close,minimize,maximize buttons and sort the app grid alphabetically on gnome shell"
 gsettings set org.gnome.desktop.peripherals.touchpad click-method 'areas'
 gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
+gsettings set org.gnome.shell app-picker-layout "[]"
 echo "Completed"
