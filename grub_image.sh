c4c6b75516c808f71651280e82a4b3ce575b46de
daboynb <106079917+daboynb@users.noreply.github.com>
2022-12-19 11:52:34 +0100

    Create grub_image.sh

---
+++ grub_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Add grub background image
+echo "Insert an image inside /boot/grub and name it splash0.png"
+read -p "Press enter when you have done"
+if [ -f /boot/grub/splash0.png ]  
+ then
+    echo "splash0.png detected"
+ else
+    echo "splash0.png is not present"
+    break
+ fi
+sudo sed -i "s@/usr/share/desktop-base/homeworld-theme/grub/grub-4x3.png@/grub/splash0.png@g" /boot/grub/grub.cfg
+sudo update-grub
+echo "done"
