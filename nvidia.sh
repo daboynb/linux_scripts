523c3a9a527630070846fb0440b7bd8dd36a0605
daboynb <106079917+daboynb@users.noreply.github.com>
2023-04-08 16:05:25 +0200

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -19,7 +19,7 @@ Choose an option:  "
 
     1)     
             sudo mkdir -p /var/lib/shim-signed/mok 
-            openssl req -new -x509 -newkey rsa:2048 -keyout /var/lib/shim-signed/mok/Nvidia.key -outform DER -out /var/lib/shim-signed/mok/Nvidia.der -nodes -days 36500 -subj "/CN=Graphics Drivers"
+            sudo openssl req -new -x509 -newkey rsa:2048 -keyout /var/lib/shim-signed/mok/Nvidia.key -outform DER -out /var/lib/shim-signed/mok/Nvidia.der -nodes -days 36500 -subj "/CN=Graphics Drivers"
             sudo mokutil --import /var/lib/shim-signed/mok/Nvidia.der
             echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf; sudo update-initramfs -u
             read -p "Now, after pressing enter it will reboot; at boot chose enroll mok --> continue --> yes --> insert the password --> reboot"
