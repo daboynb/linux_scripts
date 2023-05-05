c49a7aae7e9a944bf14b1d60c2f7baf40a3b0901
daboynb <106079917+daboynb@users.noreply.github.com>
2023-05-05 13:12:46 +0200

    Update woeusb-ng.sh

--- woeusb-ng.sh
+++ woeusb-ng.sh
@@ -2,17 +2,20 @@
 
 install_arch_linux_dependencies() {
     echo "Installing Arch Linux dependencies..."
-    sudo pacman -Syu p7zip python-pip python-wxpython
+    sudo pacman -S p7zip python-pip python-wxpython --noconfirm
+    check_installation_status
 }
 
 install_debian_dependencies() {
     echo "Installing Debian-based dependencies..."
-    sudo apt install git p7zip-full python3-pip python3-wxgtk4.0 grub2-common grub-pc-bin parted dosfstools ntfs-3g
+    sudo apt install -y git p7zip-full python3-pip python3-wxgtk4.0 grub2-common grub-pc-bin parted dosfstools ntfs-3g
+    check_installation_status
 }
 
 install_fedora_dependencies() {
     echo "Installing Fedora-based dependencies..."
-    sudo dnf install git p7zip p7zip-plugins python3-pip python3-wxpython4
+    sudo dnf install -y git p7zip p7zip-plugins python3-pip python3-wxpython4
+    check_installation_status
 }
 
 install_woeusb_ng() {
@@ -23,18 +26,27 @@ install_woeusb_ng() {
     cd ..
 }
 
+check_installation_status() {
+    if [ $? -eq 0 ]; then
+        echo "Installation successful."
+    else
+        echo "Installation failed."
+        exit 1
+    fi
+}
+
 if command -v pacman &>/dev/null; then
     echo "Arch Linux"
     install_arch_linux_dependencies
-    install_woeusb_ng
 elif command -v apt-get &>/dev/null; then
     echo "Debian-based (Debian)"
     install_debian_dependencies
-    install_woeusb_ng
 elif command -v dnf &>/dev/null; then
     echo "Fedora-based"
     install_fedora_dependencies
-    install_woeusb_ng
 else
     echo "Unknown"
+    exit 1
 fi
+
+install_woeusb_ng
