390c35e061bfcd299ae83a7bcf6ac956aac5398c
daboynb <106079917+daboynb@users.noreply.github.com>
2023-06-25 15:25:31 +0200

    Add files via upload

---
+++ upgrade_debian_11_to_12.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update the system
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+# Change the sources.list
+text="deb http://deb.debian.org/debian bookworm main contrib non-free-firmware
+deb-src http://deb.debian.org/debian bookworm main contrib non-free-firmware
+
+deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-free-firmware
+deb-src http://deb.debian.org/debian-security/ bookworm-security main contrib non-free-firmware
+ 
+deb http://deb.debian.org/debian bookworm-updates main contrib non-free-firmware
+deb-src http://deb.debian.org/debian bookworm-updates main contrib non-free-firmware
+"
+
+sudo echo "$text" | sudo tee /etc/apt/sources.list
+
+# Start upgrade
+sudo apt-get update
+sudo apt upgrade --without-new-pkgs -y
+sudo apt full-upgrade -y
