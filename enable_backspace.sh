5bd338dc86f1f6521801d4579d01c1d358ae9bfd
daboynb <106079917+daboynb@users.noreply.github.com>
2023-06-26 16:23:25 +0200

    Update enable_backspace.sh

--- enable_backspace.sh
+++ enable_backspace.sh
@@ -3,6 +3,6 @@ echo "Enable backspace"
 sudo apt-get install python3-nautilus -y
 mkdir -p ~/.local/share/nautilus-python/extensions
 cd ~/.local/share/nautilus-python/extensions
-wget https://raw.githubusercontent.com/riclc/nautilus_backspace/master/BackspaceBack.py
+wget https://raw.githubusercontent.com/jesusferm/Nautilus-43-BackSpace/main/BackSpace.py
 killall nautilus
 echo "Completed"
