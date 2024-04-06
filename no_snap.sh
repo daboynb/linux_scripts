bba3a67ce56c57083f747181a8b25561401c3813
daboynb <106079917+daboynb@users.noreply.github.com>
2024-04-06 18:04:55 +0200

    Update no_snap.sh

--- no_snap.sh
+++ no_snap.sh
@@ -14,8 +14,11 @@ sleep 1
 echo "Waiting... dpkg lock"
 done
 
-# Start
+# Update system
 sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove --purge -y
 
 echo "Removing snap...This will take a while"
 
@@ -54,25 +57,12 @@ sudo chown root:root /etc/apt/preferences.d/no-snap.pref
 rm -rf /home/"$USER"/snap
 sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
 
-# Update system
-sudo apt update
-sudo apt upgrade -y --allow-downgrades
-sudo apt dist-upgrade -y
-sudo apt autoremove --purge -y
-
 # Install brave browser
 sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg            
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
-
-# Set firefox deb
-sudo add-apt-repository ppa:mozillateam/ppa -y
-printf "Package: firefox*\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1000" >> mozilla-firefox 
-sudo mv mozilla-firefox /etc/apt/preferences.d/mozillateamppa
-sudo chown root:root /etc/apt/preferences.d/mozillateamppa
-# uncomment to install ff sudo apt install -t 'o=LP-PPA-mozillateam' firefox
 
 echo "Script completed successfully"
 
