20d4cf94f88df387d72b70f0ac96b5504d769b4e
daboynb <106079917+daboynb@users.noreply.github.com>
2024-04-06 18:02:22 +0200

    Update no_snap.sh

--- no_snap.sh
+++ no_snap.sh
@@ -60,12 +60,19 @@ sudo apt upgrade -y --allow-downgrades
 sudo apt dist-upgrade -y
 sudo apt autoremove --purge -y
 
-echo "Install brave browser"
+# Install brave browser
 sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg            
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
+
+# Set firefox deb
+sudo add-apt-repository ppa:mozillateam/ppa -y
+printf "Package: firefox*\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1000" >> mozilla-firefox 
+sudo mv mozilla-firefox /etc/apt/preferences.d/mozillateamppa
+sudo chown root:root /etc/apt/preferences.d/mozillateamppa
+# uncomment to install ff sudo apt install -t 'o=LP-PPA-mozillateam' firefox
 
 echo "Script completed successfully"
 
